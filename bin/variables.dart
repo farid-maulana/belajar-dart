5c8a00f36e782ec11bc9575dc99ba756f012a8ec
farid-maulana <faridmaulana1921@gmail.com>
2021-05-10 16:37:20 +0700

    Variables & data types

---
+++ bin/variables.dart
@@ -0,0 +1,25 @@
+void main() {
+  // Membuat variabel tanpa mendeklarasikan tipe data
+  var greetings = 'Hello Dart!'; // string
+  print(greetings);
+
+  var myAge = 21; // int
+  print(myAge);
+
+  var age; // dinamyc -> dapat diisi dengan tipe data apapun
+  age = 20;
+  print(age);
+  age = '21 tahun';
+  print(age);
+
+  // Membuat variabel dengan mendeklarasikan tipe data
+  String ucapan = 'Halo Dart!';
+  print(ucapan);
+
+  int usiaku = 21;
+  print(usiaku);
+
+  int usia;
+  usia = 20;
+  print(usia);
+}
