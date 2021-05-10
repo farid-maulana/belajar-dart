85d15cb201dab8ba0391113bfcd28d0e9a805bca
farid-maulana <faridmaulana1921@gmail.com>
2021-05-10 18:26:49 +0700

    Inputan user

---
+++ bin/inputan.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+ 
+void main() {
+  stdout.write('Nama Anda: ');
+  String name = stdin.readLineSync();
+  stdout.write('Usia Anda: ');
+  int age = int.parse(stdin.readLineSync());
+  print('Halo $name, usia Anda $age tahun');
+}
+
+/**
+ * NOTE:
+ * Jika menggunakan VSCODE harus dijalankan melalui terminal menggunakan perintah
+ * dart run bin/inputan.dart
+ * dikarenakan jika menggunakan Run pada VSCODE maka file akan dijalankan melalui
+ * debug console
+ */
