1cd50c462b3cc5b1022717493ec2ae228f8f8e00
farid-maulana <faridmaulana1921@gmail.com>
2021-05-14 14:26:39 +0700

    Konversi suhu

---
+++ bin/konversi_suhu.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main() {
+  stdout.write('Masukkan suhu dalam Fahrenheit: ');
+  var fahrenheit = num.parse(stdin.readLineSync());
+  var celcius = (fahrenheit - 32) * 5 / 9;
+  var reamur = (fahrenheit - 32) * 4 / 9;
+  var kelvin = (fahrenheit - 32) * 5 / 9 + 273;
+  print('$fahrenheit derajat Fahrenheit = $celcius derajat Celcius');
+  print('$fahrenheit derajat Fahrenheit = $reamur derajat Reamur');
+  print('$fahrenheit derajat Fahrenheit = $kelvin derajat Kelvin');
+}
